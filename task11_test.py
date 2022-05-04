2b54a1e77ee0c1afaad2013210c4433101d45508
Nikita <71011093+crawlic-stud@users.noreply.github.com>
2022-05-04 23:20:59 +0300

    Add files via upload

--- task11_test.py
+++ task11_test.py
@@ -10,7 +10,7 @@ class TaskTest(unittest.TestCase):
         self.assertEqual(find_max_weight(10, [1, 4, 8]), 9, "OK")
 
     def test_custom(self):
-        self.assertEqual(find_max_weight(20, [1, 4, 8]), 13, "OK")
+        self.assertEqual(find_max_weight(10, [1, 4, 8, 6]), 10, "OK")
         self.assertEqual(find_max_weight(50, [10, 40, 8]), 50, "OK")
         self.assertEqual(find_max_weight(45, [1, 40, 8]), 41, "OK")
         self.assertEqual(find_max_weight(200, [10, 40, 80]), 130, "OK")
